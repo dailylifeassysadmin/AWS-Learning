66e4d710e5e40c68d9430635dba364fb8ed17b4b
dailylifeassysadmin <raviindia77@hotmail.com>
2025-08-28 05:26:17 +0530

    Adding: added a new main.tf as part of my day 2.

---
+++ Terraform/Day-02/README.md
@@ -0,0 +1,7 @@
+## This is day 02 of my Terraform journey
+
+I will be learning and sharing the basics of terrafom with AWS (Amazon Web Sevices) cloud platform.
+
+---
+
+**Today, I am working on AWS S3 bucket creation and its dieerent options.**
---
+++ Terraform/Day-02/main.tf
@@ -0,0 +1,14 @@
+# add AWS provider block here
+provider "aws" {
+  region = "us-east-1"
+}
+
+# add AWS S3 bucket resource block here
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-12345-day02-tf-example"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
